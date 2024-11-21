ade8b3b61cc27d8a0c04f5e08e2c91afe308a38e
Lipatov.Ilya <lipatoff.ilya99@mail.ru>
2024-11-21 23:31:29 +0300

    fix (step 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,20 +20,19 @@ application {
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    annotationProcessor("org.apache.commons:commons-lang3:3.12.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("org.slf4j:slf4j-log4j12:2.0.3")
-    testImplementation("org.assertj:assertj-core:3.23.1")
     implementation("info.picocli:picocli:4.7.6")
-    annotationProcessor("info.picocli:picocli-codegen:4.6.3")
-
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.jacoco:org.jacoco.agent:0.8.10")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+//    testImplementation("org.assertj:assertj-core:3.23.1")
+//    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+//    annotationProcessor("info.picocli:picocli-codegen:4.6.3")
+//    testImplementation("org.slf4j:slf4j-log4j12:2.0.3")
 }
 
 tasks.test {
